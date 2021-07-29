acbd6ffe3436cc7512122792dbd595c911453345
nikhilesh <jnikhilesh@gmail.com>
2021-07-29 15:45:08 +0530

    Commit for branch

---
+++ AppletExample.java
@@ -0,0 +1,90 @@
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/* 
+<applet code="AppletExample" width="500" height="200">
+</applet>
+*/
+  
+public class AppletExample extends JApplet 
+{
+
+public void init()
+{
+add(new B());  	//Adding a JPanel to this Swing applet
+}
+
+}
+
+
+
+class B extends JPanel implements ActionListener
+{
+JLabel jb;
+JButton Box1, box2, box3, box4;
+String str;
+
+B()
+{
+jb= new JLabel("Welcome, please click on any button to unbox some interesting knowledge -");
+Box1 = new JButton("Box1");
+box2 = new JButton("Box2");
+box3 = new JButton("Box3");
+box4 = new JButton("Box4");
+
+str ="";
+
+setLayout(new FlowLayout());
+
+add(jb);
+add(Box1);
+add(box2);
+add(box3);
+add(box4);
+
+Box1.addActionListener(this);
+box2.addActionListener(this);
+box3.addActionListener(this);
+box4.addActionListener(this);
+}
+
+
+
+public void actionPerformed(ActionEvent ae)
+{
+if(ae.getActionCommand().equals("Box1"))
+{
+str="Amazon is the largest tropical rain forest, covering 40% of the South America Continent.";
+repaint();
+}
+
+if(ae.getActionCommand().equals("Box2"))
+{
+str="The Mariana Trench is the deepest point in Earth's ocean, with depth of over 10,994 metres.";
+repaint();
+}
+
+if(ae.getActionCommand().equals("Box3"))
+{
+str="The coldest temperature ever recorded was -128.6F in Antarctica, on July 21, 1983.";
+repaint();
+}
+
+if(ae.getActionCommand().equals("Box4"))
+{
+str="The oldest person to climb Mt. Everent was Japanese Miura Yiuchiro, at the age of 80.";
+repaint();
+}
+
+}
+
+
+public void paintComponent(Graphics g)
+{
+super.paintComponent(g);
+g.drawString(str, 2, 170);
+}
+
+}
